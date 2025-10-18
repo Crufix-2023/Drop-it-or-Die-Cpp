afdf0c47813ac5369a90251e8df6fe76be82d2a0
Crufix <155760866+Crufix-2023@users.noreply.github.com>
2025-10-18 20:04:18 +0300

    вынес include'ы в отдельный файл

---
+++ src/includes.h
@@ -0,0 +1,8 @@
+#include <iostream>
+#include <string>
+#include <random>
+#include <chrono>
+#include <thread>
+
+#include <TGUI/TGUI.hpp>
+#include <TGUI/Backend/SFML-Graphics.hpp>
--- src/main.cpp
+++ src/main.cpp
@@ -1,11 +1,6 @@
-#include <iostream>
-#include <string>
-#include <random>
-#include <chrono>
-#include <thread>
+/////////////////////
 
-#include <TGUI/TGUI.hpp>
-#include <TGUI/Backend/SFML-Graphics.hpp>
+#include <includes.h>
 
 /////////////////////
 
